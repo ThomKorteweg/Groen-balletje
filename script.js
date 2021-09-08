20a449f1db9368988794d1da67f043f9303edc0f
thomkorteweg <74902263+ThomKorteweg@users.noreply.github.com>
2021-09-08 10:50:02 +0000

    dvd

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 var x, y;
 
 function setup() {
-	createCanvas(500, 400);
+	createCanvas(400, 400);
 
   x = 0;
-  y = 200;
-  velocity = 5
+  y = 50;
+  vx = 5
+  vy = 5
 }
 
 function draw() {
-	background(0,100,0);
+	background(400,100,0);
   
   ellipse(x,y,50,50);
   fill(0,0,100)
-  x=x+velocity;
-
-  if(x<=0 || x > 500){
-    velocity = velocity * -1;
+  x=x + vx;
+  y=y + vy;
+  if(x<=0 || x> 400){
+    vx = vx * -1;
   }
+    if(y<=0 || y> 400){
+    vy = vy * -1;
+  }
+ 
+ 
+
 }
 
